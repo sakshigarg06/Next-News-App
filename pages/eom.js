6d2effc2a480e2c88d514aad9f37ed96ebe9047f
Sakshi Garg <sakshi@Sakshis-MacBook-Pro.local>
2022-04-19 16:21:39 +0530

    init

---
+++ pages/eom.js
@@ -0,0 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+
+import styles from "../styles/EOM.module.css";
+import { Toolbar } from "../components/toolbar";
+
+export const EOM = ({ employee }) => {
+  return (
+    <div className="page-container">
+      <Toolbar />
+
+      <div className={styles.main}>
+        <h1>Employee Of The Month</h1>
+
+        <div className={styles.employeeOfTheMonth}>
+          <h3>{employee.name}</h3>
+          <h6>{employee.position}</h6>
+          <img src={employee.image} alt="Employee Of The Month" />
+          <p>{employee.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (PageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth"
+  );
+
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee,
+    },
+  };
+};
+
+export default EOM;
---
+++ pages/feed/[pageid].js
@@ -0,0 +1,87 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "../../styles/Feed.module.css";
+import { useRouter } from "next/router";
+import { Toolbar } from "../../components/toolbar";
+
+export const Feed = ({ pageNumber, articles }) => {
+  const router = useRouter();
+
+  return (
+    <div className="page-container">
+      <Toolbar/>
+      <div className={styles.main}>
+        {articles.map((article, index) => (
+          <div key={index} className={styles.post}>
+            <h1 onClick={() => (window.location.href = article.url)}>
+              {article.title}
+            </h1>
+            <p>{article.description}</p>
+            {!!article.urlToImage && (
+              <img src={article.urlToImage} alt="image" />
+            )}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.paginator}>
+        <div
+          onClick={() => {
+            if (pageNumber > 1) {
+              router.push(`/feed/${pageNumber - 1}`);
+            }
+          }}
+          className={pageNumber === 1 ? styles.disabled : styles.active}
+        >
+          Previous Page
+        </div>
+
+        <div>#{pageNumber}</div>
+
+        <div
+          onClick={() => {
+            if (pageNumber < 5) {
+              router.push(`/feed/${pageNumber + 1}`);
+            }
+          }}
+          className={pageNumber === 5 ? styles.disabled : styles.active}
+        >
+          Next Page
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const pageNumber = pageContext.query.pageid;
+
+  if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
+    return {
+      props: {
+        articles: [],
+        pageNumber: 1,
+      },
+    };
+  }
+
+  const apiResponse = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=in&pageSize=5&page=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
+    }
+  ).then((res) => res.json());
+
+  const { articles } = apiResponse;
+
+  return {
+    props: {
+      articles: articles,
+      pageNumber: Number.parseInt(pageNumber),
+    },
+  };
+};
+
+export default Feed;
